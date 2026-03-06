9042a7c4657726b522c773dec04de79d456bc002
drew <7977907+notnotdrew@users.noreply.github.com>
2026-03-05 17:30:16 -0700

    Create courses

---
+++ prisma/migrations/20260306022934_create_courses/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "courses" (
+    "id" UUID NOT NULL,
+    "slug" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "courses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "courses_slug_key" ON "courses"("slug");
+
+-- CreateIndex
+CREATE INDEX "courses_slug_idx" ON "courses"("slug");
