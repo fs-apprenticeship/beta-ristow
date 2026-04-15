0a5b219652a6466edbc48e6cbee78f1fdce61b3f
Al Cerdan Lico <alcerdan@yahoo.com>
2026-04-15 14:13:12 -0700

    add account id to challenge submission

--- src/features/codechallenge/save-submission.ts
+++ src/features/codechallenge/save-submission.ts
@@ -1,3 +1,4 @@
+import getCurrentUserId from "@/lib/clerk/get-current-user-id";
 import getClient from "@/lib/prisma/get-client";
 
 interface SaveSubmissionInput {
@@ -7,9 +8,10 @@ interface SaveSubmissionInput {
 
 export async function saveSubmission({
   challengeId,
-  userCode,
+  userCode
 }: SaveSubmissionInput) {
   const prisma = getClient();
+
   if (!challengeId) {
     throw new Error("Missing challengeId");
   }
@@ -18,8 +20,21 @@ export async function saveSubmission({
     throw new Error("Missing userCode");
   }
 
+  const clerkUserId = await getCurrentUserId();
+
+  if (!clerkUserId) {
+    throw new Error("Unauthorized!");
+  }
+
+  const account = await prisma.account.findUniqueOrThrow({
+    where: {
+      clerkUserId,
+    },
+  });
+
   const submission = await prisma.challengeSubmission.create({
     data: {
+      accountId: account.id,
       challengeId,
       correct: false,
       feedback: "Pending evaluation",
