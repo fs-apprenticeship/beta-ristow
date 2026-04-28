0d5d611ecddeeef36a374147779d7cb8a8dcbf8f
ISTAFAMARSHALL <istafa.a.marshall@gmail.com>
2026-04-28 11:45:33 -0400

    refactor(quizzes): align getQuizById result with persisted quiz type

--- src/features/quiz/data/get-quiz-by-id.ts
+++ src/features/quiz/data/get-quiz-by-id.ts
@@ -5,19 +5,14 @@ const prisma = getClient();
 export default async function getQuizById(quizId: string) {
   return await prisma?.quiz.findUnique({
     include: {
-      lesson: {
-        include: {
-          lessonArticle: true,
-        },
-      },
-      questions: {
-        include: {
-          options: {
+        questions: {
+            include: {
+                options: {
+                    orderBy: { position: "asc" },
+                },
+            },
             orderBy: { position: "asc" },
-          },
         },
-        orderBy: { position: "asc" },
-      },
     },
     where: { id: quizId },
   });
