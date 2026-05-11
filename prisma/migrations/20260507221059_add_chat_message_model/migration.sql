170463a41b140b7979c091566920fb86af4e3ff2
drew <7977907+notnotdrew@users.noreply.github.com>
2026-05-11 14:00:10 -0600

    Revert all commits after 15d27e2

--- prisma/migrations/20260507221059_add_chat_message_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `updated_at` to the `chat_messages` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "chat_messages" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
--- prisma/migrations/20260507223135_add_learner_id_index_to_chat_message/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "chat_messages_learner_id_idx" ON "chat_messages"("learner_id");
